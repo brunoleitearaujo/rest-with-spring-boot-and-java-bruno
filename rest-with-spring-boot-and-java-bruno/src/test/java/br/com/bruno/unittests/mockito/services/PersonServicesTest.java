846a727a09a2a7cf2c386d5e27276d90d55de3eb
brunoleitearaujo <bruno.araujo1989@gmail.com>
2024-08-31 18:59:11 -0300

    Implementando links HATEOAS com Mockito e JUnit

---
+++ rest-with-spring-boot-and-java-bruno/src/test/java/br/com/bruno/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,170 @@
+package br.com.bruno.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.bruno.data.vo.v1.PersonVO;
+import br.com.bruno.exceptions.RequiredObjectsIsNullException;
+import br.com.bruno.model.Person;
+import br.com.bruno.repositories.PersonRepository;
+import br.com.bruno.services.PersonServices;
+import br.com.bruno.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+
+	MockPerson input;
+
+	@InjectMocks
+	private PersonServices service;
+
+	@Mock
+	private PersonRepository repository;
+
+	@BeforeEach
+	void setUpMocks() throws Exception {
+		input = new MockPerson();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testFindAll() {
+		List<Person> list = input.mockEntityList();
+
+		when(repository.findAll()).thenReturn(list);
+
+		var people = service.findAll();
+		
+		assertNotNull(people);
+		assertEquals(14, people.size());
+
+		people.forEach(person -> {
+			Long key = person.getKey();
+			assertNotNull(person);
+			assertNotNull(key);
+			assertNotNull(person.getLinks());
+
+			assertTrue(person.toString().contains("[</api/person/v1/" + key + ">;rel=\"self\"]"));
+
+			assertEquals("Addres Test" + key, person.getAddress());
+			assertEquals("First Name Test" + key, person.getFirstName());
+			assertEquals("Last Name Test" + key, person.getLastName());
+			assertEquals(((key % 2)==0) ? "Male" : "Female", person.getGender());
+		});
+	}
+
+	@Test
+	void testFindById() {
+		Person entity = input.mockEntity(1);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		
+		var result = service.findById(1L);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		assertTrue(result.toString().contains("[</api/person/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+
+	@Test
+	void testCreate() {
+		Person entity = input.mockEntity(1);
+
+		Person persisted = input.mockEntity(1);
+
+		PersonVO vo = input.mockVO(1);
+
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.create(vo);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		assertTrue(result.toString().contains("[</api/person/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+
+	@Test
+	void testCreateWithNullPerson() {
+		Exception exception = assertThrows(
+				RequiredObjectsIsNullException.class, () -> service.create(null));
+
+		String expectedMessage = "It is not allowed to persist a null object!";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.contains(actualMessage));
+	}
+
+	@Test
+	void testUpdate() {
+		Person entity = input.mockEntity(1);
+
+		Person persisted = input.mockEntity(1);
+
+		PersonVO vo = input.mockVO(1);
+
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.update(vo);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		assertTrue(result.toString().contains("[</api/person/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+
+	@Test
+	void testUpdateWithNullPerson() {
+		Exception exception = assertThrows(
+				RequiredObjectsIsNullException.class, () -> service.update(null));
+
+		String expectedMessage = "It is not allowed to persist a null object!";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.contains(actualMessage));
+	}
+
+	@Test
+	void testDelete() {
+		Person entity = input.mockEntity(1);
+
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
+		repository.findById(1L);
+	}
+
+}
